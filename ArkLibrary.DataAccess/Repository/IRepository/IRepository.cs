0b080f74b44af782f3aae797a5f8870b44c17dac
Ivaylo <ivaylonikolov.n@gmail.com>
2023-11-13 19:05:01 +0200

    Initial whole project testing commit

---
+++ ArkLibrary.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArkWeb.DataAccess.Repository.IRepository
+{
+	public interface IRepository<T> where T : class
+	{
+		//T - Category or any other generic model on which we perform crud or interact with dbcontex
+
+		IEnumerable<T> GetAll(string? includeProperties = null);
+		T Get(Expression<Func<T,bool>> filter, string? includeProperties = null);
+
+		void Add(T entity);
+		void Delete(T entity);
+		void DeleteRange(IEnumerable<T> entity);
+
+	}
+}
---
+++ ArkWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,124 @@
+﻿using ArkLibrary.Utility;
+using ArkWeb.DataAccess.Data;
+using ArkWeb.DataAccess.Repository;
+using ArkWeb.DataAccess.Repository.IRepository;
+using ArkWeb.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArkLibrary.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString()) 
+            //{
+            //	ModelState.AddModelError("name","The DisplayOrder cannot exactly match the Name");
+            //}
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.CategoryId == id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);   Ways to retrive a record !
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault(); Ways to retrive a record ! Only use when we need filtering of data then FirstOrDefault
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString()) 
+            //{
+            //	ModelState.AddModelError("name","The DisplayOrder cannot exactly match the Name");
+            //}
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.CategoryId == id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);   Ways to retrive a record !
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault(); Ways to retrive a record ! Only use when we need filtering of data then FirstOrDefault
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+
+            Category obj = _unitOfWork.Category.Get(u => u.CategoryId == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Category.Delete(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
